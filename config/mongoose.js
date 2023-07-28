75bd6063d2407b4db5db6bb5ba9c41cd75cd08cc
AmanRajwar <135263136+AmanRajwar@users.noreply.github.com>
2023-07-29 02:12:35 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-
-})
+mongoose.connect('mongodb://127.0.0.1/placement_cell');
 const db =mongoose.connection;
 db.on('error',console.error.bind(console,"error connecting to the database"));
 
